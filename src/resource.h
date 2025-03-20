4084dd0091a7e49d39f3bb9cf5da040a38d81661
Alex Golubev <9640178+povargek@users.noreply.github.com>
2025-03-20 06:49:52 +0300

    add settings dialog && icon

--- src/resource.h
+++ src/resource.h
@@ -1,8 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Resource.rc
+//
+#define IDI_ICON_KEKA                   5
+#define IDD_SETTINGS_DIALOG             102
+#define IDI_ICON1                       107
+#define IDC_COMBO1                      1001
+#define IDC_TAB1                        1002
+#define IDC_COMMAND1                    1003
+#define IDC_EDIT1                       1004
+#define IDC_BTN_LOGS_FOLDER_SELECT      1005
+#define IDC_CHECK_LOG_TO_FILE           1006
+#define IDD_BTN_APPLY                   1007
+#define IDD_BTN_START_GAME              1008
+#define IDC_LINK_RESET_TO_GAME_FOLDER_LOGS_STORAGE 1009
+#define IDC_LINK_RESET_TO_APPDATA_LOGS_STORAGE 1010
+#define IDC_TEXT_LOGGING_BASE           -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
